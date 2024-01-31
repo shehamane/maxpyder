48a8cc9f79104723c44ed53d20006c4e80e66e5b
lopastin <45077025+lopastin@users.noreply.github.com>
2024-02-01 00:55:05 +0300

    shablons

--- app/backend/Serv.go
+++ app/backend/Serv.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 	"os"
 )
 
+var tpl = template.Must(template.ParseFiles("app/backend/templates/index.html"))
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello World!<h1>"))
+	tpl.Execute(w, nil)
 }
 
 func main() {
